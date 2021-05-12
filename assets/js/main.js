495be3da4ffa8eaf9d22cbdefcbacd79256bcc2d
Anderson Costa <anderson@7play.com.br>
2021-05-12 13:48:49 -0300

    Adicionando a pagina de obrigado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $(function () {
     // Links para o Checkout
-    var linkCheckout = 'https://pay.hotmart.com/N51854403W?off=wlvonm69';
+    var linkCheckout = 'https://pay.hotmart.com/N51854403W?off=sx48f02w&checkoutMode=10&split=10';
     var url_string = window.location.href;
     var urlparams = new URL(url_string);
     var sck = urlparams.searchParams.get("sck");
 
-    var url = linkCheckout + '?sck=' + sck;
+    var url = linkCheckout + '&sck=' + sck;
 
     $('.checkout').wrap($('<a rel="noreferrer" target="_blank">').attr('href', url));
 
@@ -35,8 +35,8 @@ $(function () {
 
     //Rotator do Whatsapp
     var atendente = []
-    atendente[0] = 'https://api.whatsapp.com/send?phone=5521990802711&text=Eu%20quero%20saber%20mais%20sobre%20o%20Curso%20de%20Conversão'
-    atendente[1] = 'https://api.whatsapp.com/send?phone=5521994292234&text=Eu%20quero%20saber%20mais%20sobre%20o%20Curso%20de%20Conversão'
+    atendente[0] = 'https://api.whatsapp.com/send?phone=5521990802711&text=Oii!%20Eu%20estava%20no%20P%C3%A1gina%20de%20Vendas%20e%20Fiquei%20com%20Obje%C3%A7%C3%B5es!'
+    //atendente[1] = 'https://api.whatsapp.com/send?phone=5521994292234&text=Oii!%20Eu%20estava%20no%20P%C3%A1gina%20de%20Vendas%20e%20Fiquei%20com%20Obje%C3%A7%C3%B5es!'
 
     var random = Math.floor(Math.random() * atendente.length);
 
